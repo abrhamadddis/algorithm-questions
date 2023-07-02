4b3174dfecd1ee629df56bb0eeef1db363eca18e
abrhamadddis <abrhamaddis32@gmail.com>
2023-07-02 21:46:15 +0300

    creting java environment

---
+++ addition.java
@@ -0,0 +1,13 @@
+class addition{
+    public static void main(String[] args){
+        int x = 1;
+        int y = 5;
+        int output = addNums(x, y);
+        System.out.println(output);
+    } 
+    // a function that return sum of two numbers
+    public static int addNums(int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+}
+}
---
+++ helloWorld.java
@@ -0,0 +1,7 @@
+// pogram that return hello world
+
+class helloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello java");
+    }
+}
